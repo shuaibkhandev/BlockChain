93e7fed130bc78069f7df2a22a7923ad6b548d84
shuaibdev <skhan.csit@gmail.com>
2025-06-16 17:53:17 +0500

    created the distributing blocks logic

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const BlockChain = require("./blockchain");
-const PORT = 8000;
+const PubSub = require("./pubSub");
+
 const blockchain = new BlockChain();
-
-
+const pubsub = new PubSub({blockchain})
 
 app.use(bodyParser.json());
+
+setTimeout(()=>{
+    pubsub.broadcastChain()
+},1000)
+
 
 app.get('/api/blocks', (req, res)=>{
     res.send(blockchain);
@@ -16,8 +21,19 @@ app.get('/api/blocks', (req, res)=>{
 app.post("/api/block", (req, res)=>{
     const {data} = req.body;    
     blockchain.addBlock({data});
+       pubsub.broadcastChain()
     res.redirect('/api/blocks');
 })
+
+
+const DEFAULT_PORT = 8000;
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT==='true'){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, ()=>{
     console.log(`Server start at port ${PORT}`);  
